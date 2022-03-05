366c8409241e2ad33b2212024c6e81ddcd70a1fd
Vlad <vlad@pop-os.localdomain>
2022-03-05 03:31:44 +0200

    added function to get game genres

--- src/api/index.js
+++ src/api/index.js
@@ -7,8 +7,14 @@ export class Rawg {
     this.default = (endpoint) => { return `https://api.rawg.io/api/${endpoint + (endpoint.includes('?') ? '&' : '?')}key=${this.key}` };
   }
 
-  async getGames() {
-    const result = await axios.get(this.default('games?page_size=50'));
+  async getGames(limit, ordering) {
+    const result = await axios.get(this.default(`games?page_size=${limit}&ordering=${ordering}`));
+
+    return result.data.results;
+  }
+
+  async getGenres() {
+    const result = await axios.get(this.default('genres?count=1'));
 
     return result.data.results;
   }
