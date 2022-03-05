4b53459815573ef2211d9b94981437cbc83949c3
Vlad <vlad@pop-os.localdomain>
2022-03-05 03:32:38 +0200

    made it full width

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -4,16 +4,14 @@ import { Flex } from '@chakra-ui/react';
 export const MainContent = () => {
   return(
     <Flex
-      direction='row'
+      direction='column'
       justify='center'
       gap='5'
       flexWrap='wrap'
       p='5'
       bg='gray.800'
-      shadow='xl'
-      w={['full', 'full', '80%']}
-      mb='10'
-      rounded='sm'
+      zIndex='10'
+      w={['full', 'full', 'full']}
     >
       <Outlet />
     </Flex>
